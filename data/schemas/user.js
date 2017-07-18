e04103e6c93f44b1764e7e23d2b66f7a372c5d21
Piotr Kowalski <piet.kowalski@gmail.com>
2017-07-18 23:04:26 +0100

    Clean user schema. Add is-user-active flag

--- data/schemas/user.js
+++ data/schemas/user.js
@@ -1,40 +1,8 @@
 var mongoose = require('mongoose');
-var request = require('request');
 var bcrypt = require('bcrypt-nodejs');
 var emailRegexp = /.+\@.+\..+/;
-var TIMESPAN_YEAR = 31536000000;
-var TIMESPAN_18_YEARS = 18 * TIMESPAN_YEAR;
-
-function validate_18_years_old_or_more(date) {
-    return (Date.now() - date.getTime()) > TIMESPAN_18_YEARS;
-}
-
-function twitterHandleExists(handle, done) {
-    request('http://twitter.com/' + encodeURIComponent(handle), function (err, res) {
-        if (err) {
-            console.error(err);
-            return done(false);
-        }
-        if (res.statusCode > 299) {
-            done(false);
-        } else {
-            done(true);
-        }
-    });
-}
 
 mongoose.Promise = require('bluebird');
-
-function filterTwitterHandle(handle) {
-    if (!handle) {
-        return;
-    }
-    handle = handle.trim();
-    if (handle.indexOf('@') === 0) {
-        handle = handle.substring(1);
-    }
-    return handle;
-}
 
 var UserSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
@@ -43,27 +11,11 @@ var UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        unique: true,
         match: emailRegexp
     },
-    gender: {
-        type: String,
-        required: true,
-        uppercase: true,
-        'enum': ['M', 'F']
-    },
-    birthday: {
-        type: Date,
-        validate: [validate_18_years_old_or_more, 'You must be 18 years old or more']
-    },
-    bio: {
-        type: String
-    },
-    twitter: {
-        type: String,
-        sparse: true,
-        validate: [twitterHandleExists, 'Please provide a valid twitter handle'],
-        get: filterTwitterHandle,
-        set: filterTwitterHandle
+    active: {
+        type: Boolean
     },
     meta: {
         created_at: {
@@ -94,13 +46,6 @@ UserSchema
             first: nameComponents.join(' ')
         };
     });
-UserSchema
-    .virtual('twitter_url')
-    .get(function () {
-        if (this.twitter) {
-            return 'http://twitter.com/' + encodeURIComponent(this.twitter);
-        }
-    });
 UserSchema.pre('save', function (next) {
 
     var that = this;
@@ -114,7 +59,7 @@ UserSchema.pre('save', function (next) {
         return next();
     }
 
-    bcrypt.hash(this.password, null, null, function (err, hash) {
+    bcrypt.hash(that.password, null, null, function (err, hash) {
         if (err) {
             return next(err);
         }
@@ -132,13 +77,5 @@ UserSchema.methods.comparePassword = function (candidatePassword, callback) {
 };
 
 UserSchema.index({username: 1, 'meta.created_at': -1});
-
-UserSchema.methods.recentArticles = function (callback) {
-    return this.model('Article')
-        .find({author: this._id})
-        .sort('created_at')
-        .limit(5)
-        .exec(callback);
-};
 
 module.exports = UserSchema;
