465d6b021d57e6d735a76866eee061f63722b23d
Piotr Kowalski <piet.kowalski@gmail.com>
2017-11-13 05:53:19 +0000

    Refactor - be able to get all planes

--- routes/bookings/planes.js
+++ routes/bookings/planes.js
@@ -1,58 +1,76 @@
-var async = require('async');
-var Plane = require('../../data/models/flight');
-var loggedIn = require('../middleware/logged_in');
-var loadPlane = require('../middleware/load_plane');
-var max_per_page = 10;
+const async = require('async');
+const Plane = require('../../data/models/flight');
+const loggedIn = require('../middleware/logged_in');
+const loadPlane = require('../middleware/load_plane');
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
 
-var express = require('express');
-var router = express.Router();
-var mongoose = require('mongoose');
+router.get('/', loggedIn, (req, res, next) => {
 
-router.get('/', loggedIn, function (req, res, next) {
-
-    var current_page = req.query.page && parseInt(req.query.page, 10) || 1;
-    var user_id = req.user._id;
-    var param_type = req.query.type || '';
-    var sort = {'departure_date': 1};
-    var type = 'Current';
-    var type_lower = param_type.toLowerCase() || type.toLowerCase();
-    var newDate = new Date();
+    const currentUser = req.user._id;
+    const currentPage = (req.query.page && parseInt(req.query.page, 10)) || 1;
+    const currentType = (req.query.type || '').toLowerCase();
+    const currentLimit = (req.query.limit && parseInt(req.query.limit, 10)) || 10;
+    const newDate = new Date();
     newDate.setHours(0, 0, 0, 0);
-    var match = {
-        $or: [
-            {"departure_date": {$gte: newDate}},
-            {"return_departure_date": {$gte: newDate}}
-        ],
-        "created_by": user_id,
-    };
-    if ('past' === type_lower) {
-        sort = {'departure_date': -1};
-        type = 'Past';
-        match = {
-            $and: [
-                {"departure_date": {$lt: newDate}},
-                {
-                    $or: [
-                        {"return_departure_date": {$lt: newDate}},
-                        {"return_departure_date": {$eq: null}},
-                        {"return_departure_date": {$eq: ""}}
-                    ]
-                }
-            ],
-            "created_by": user_id,
-        };
+
+    let type;
+    let sort;
+    let match;
+
+    switch (currentType) {
+        case 'current':
+            sort = {'departure_date': 1};
+            type = 'Current';
+            match = {
+                $or: [
+                    {"departure_date": {$gte: newDate}},
+                    {"return_departure_date": {$gte: newDate}}
+                ],
+                "created_by": currentUser,
+            };
+
+            break;
+
+        case 'past':
+            sort = {'departure_date': -1};
+            type = 'Past';
+            match = {
+                $and: [
+                    {"departure_date": {$lt: newDate}},
+                    {
+                        $or: [
+                            {"return_departure_date": {$lt: newDate}},
+                            {"return_departure_date": {$eq: null}},
+                            {"return_departure_date": {$eq: ""}}
+                        ]
+                    }
+                ],
+                "created_by": currentUser,
+            };
+
+            break;
+
+        default:
+            sort = {'departure_date': -1};
+            type = 'All';
+            match = {created_by: currentUser};
+
+            break;
     }
+
     req.active = 'planes';
 
     async.parallel(
         [
-            function (next) {
+            (next) => {
                 Plane.aggregate(
                     [
                         {$match: match},
                         {"$sort": sort},
-                        {"$skip": ((current_page - 1) * max_per_page)},
-                        {"$limit": max_per_page},
+                        {"$skip": ((currentPage - 1) * currentLimit)},
+                        {"$limit": currentLimit},
                         {
                             $project: {
                                 "_id": 1,
@@ -130,7 +148,7 @@ router.get('/', loggedIn, function (req, res, next) {
                     }
                 );
             },
-            function (next) {
+            (next) => {
                 Plane.aggregate(
                     [
                         {$match: match},
@@ -156,7 +174,7 @@ router.get('/', loggedIn, function (req, res, next) {
                             }
                         }
                     ],
-                    function (err, results) {
+                    (err, results) => {
                         if (err) {
                             return next(err);
                         }
@@ -168,46 +186,44 @@ router.get('/', loggedIn, function (req, res, next) {
                 );
             }
         ],
-        function (err, results) {
+        (err, results) => {
             if (err) {
                 return next(err);
             }
-            var flights = results[0];
-            var cost = 0, average_cost = 0, flights_length = 0, return_flights_length = 0;
-            if (results[1]) {
-                var aggregated = results[1];
-                cost = aggregated.cost;
-                average_cost = aggregated.avg_cost;
-                flights_length = aggregated.flights_length;
-                return_flights_length = aggregated.return_flights_length;
-            }
+
+            const journeys = results[0];
+            const journeysExist = undefined !== results[1];
+            const cost = journeysExist ? results[1].cost : 0;
+            const averageCost = journeysExist ? results[1].avg_cost : 0;
+            const journeysLength = journeysExist ? results[1].flights_length : 0;
+            const returnJourneysLength = journeysExist ? results[1].return_flights_length : 0;
 
             res.send(JSON.stringify({
                 title: type + ' flights',
-                flights: flights,
-                current_page: current_page,
-                is_first_page: current_page === 1,
-                is_last_page: current_page * max_per_page >= flights_length,
-                pages_count: flights_length <= max_per_page ? 1 : Math.ceil(flights_length / max_per_page),
-                max_per_page: max_per_page,
-                total_cost: flights_length ? (cost / 100).toFixed(2) : '0.00',
-                average_cost: flights_length ? (average_cost / 100).toFixed(2) : '0.00',
-                flights_length: flights_length,
-                return_flights_length: return_flights_length,
-                active: type_lower,
+                flights: journeys,
+                current_page: currentPage,
+                is_first_page: currentPage === 1,
+                is_last_page: currentPage * currentLimit >= journeysLength,
+                pages_count: journeysLength <= currentLimit ? 1 : Math.ceil(journeysLength / currentLimit),
+                max_per_page: currentLimit,
+                total_cost: journeysLength ? (cost / 100).toFixed(2) : '0.00',
+                average_cost: journeysLength ? (averageCost / 100).toFixed(2) : '0.00',
+                flights_length: journeysLength,
+                return_flights_length: returnJourneysLength,
+                active: currentType,
                 selected: 'planes'
             }));
         }
     );
 });
 
-router.get('/:id', loggedIn, loadPlane, function (req, res) {
+router.get('/:id', loggedIn, loadPlane, (req, res) => {
     res.send(JSON.stringify(req.plane));
 });
 
-router.put('/:id', loggedIn, loadPlane, function(req, res) {
+router.put('/:id', loggedIn, loadPlane, (req, res) => {
     const plane = req.body;
-    Plane.update(plane, function (err) {
+    Plane.update(plane, (err) => {
         if (err) {
             throw new Error(err);
         }
@@ -216,11 +232,11 @@ router.put('/:id', loggedIn, loadPlane, function(req, res) {
     });
 });
 
-router.post('/', loggedIn, function (req, res, next) {
+router.post('/', loggedIn, (req, res, next) => {
 
     var plane = req.body;
     plane.created_by = req.user._id;
-    Plane.create(plane, function (err) {
+    Plane.create(plane, (err) => {
         if (err) {
             if (err.code === 11000) {
                 res.status(409).send(JSON.stringify({ok: false, err: err}));
