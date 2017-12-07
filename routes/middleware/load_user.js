b098aa5f901b1e0f7494bdf6e5fa786abfc49294
Piotr Kowalski <piet.kowalski@gmail.com>
2017-12-07 05:08:18 +0000

    Cleanup load_user.js

--- routes/middleware/load_user.js
+++ routes/middleware/load_user.js
@@ -1,15 +1,15 @@
-var User = require('../../data/models/user');
-var ObjectId = require('mongoose').Types.ObjectId;
-function loadUser(req, res, next) {
-    User.findOne({_id: new ObjectId(req.params.id)}, function (err, user) {
+const User = require('../../data/models/user');
+const ObjectId = require('mongoose').Types.ObjectId;
+
+module.exports = (req, res, next) => {
+    User.findOne({_id: new ObjectId(req.params.id)}, (err, user) => {
         if (err) {
             return next(err);
         }
         if (!user) {
-            return res.status(404).send('Not found');
+            return res.status(404).send('User not found');
         }
         req.user = user;
         next();
     });
-}
-module.exports = loadUser;
+};
