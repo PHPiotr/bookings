3994fce4019e9969abd9da86c830551015d6cf6e
Piotr Kowalski <piet.kowalski@gmail.com>
2017-12-09 14:58:42 +0000

    Use test user in auth tests

--- test/auth.test.js
+++ test/auth.test.js
@@ -3,12 +3,80 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../app');
 const should = chai.should();
+const jwt = require('jsonwebtoken');
 
 const server = app.server;
 
 chai.use(chaiHttp);
 
 describe('Auth', () => {
+
+    const username = '__hello__';
+    const password = '__hello__';
+    const basic = new Buffer(username + ':' + password).toString('base64');
+    const body = {
+        suppressEmail: true,
+        registration: {
+            username,
+            password,
+            email: 'hello@example.com',
+            repeatPassword: password,
+        },
+    };
+    let userId;
+    let loginToken;
+    let activationToken;
+
+    const cleanup = (done) => {
+        chai.request(server)
+            .get(`${process.env.API_PREFIX}/users/${username}`)
+            .end((err, res) => {
+                if (err) {
+                    return done();
+                }
+                if (res.statusCode !== 200) {
+                    return done();
+                }
+                loginToken = jwt.sign({
+                    sub: res.body._id,
+                    purpose: 'login'
+                }, process.env.AUTH_SECRET, {algorithm: 'HS256'});
+
+                chai.request(server)
+                    .delete(`${process.env.API_PREFIX}/users/${res.body.username}`)
+                    .set('Authorization', `Bearer ${loginToken}`)
+                    .end(() => done());
+            });
+    };
+
+    before(done => cleanup(done));
+
+    beforeEach((done) => {
+        chai.request(server).post(`${process.env.API_PREFIX}/users`).send(body).end((err, res) => {
+            if (res.statusCode === 201) {
+                const location = res.get('Location');
+                const parts = location.split('/');
+                userId = parts[parts.length - 1];
+                loginToken = jwt.sign({
+                    sub: userId,
+                    purpose: 'login'
+                }, process.env.AUTH_SECRET, {algorithm: 'HS256'});
+                activationToken = jwt.sign({
+                    sub: userId,
+                    purpose: 'activation'
+                }, process.env.AUTH_SECRET, {algorithm: 'HS256'});
+                chai.request(server)
+                    .put(`${process.env.API_PREFIX}/users/${userId}`)
+                    .set('Authorization', `Bearer ${activationToken}`)
+                    .end(() => done());
+            } else {
+                done();
+            }
+        });
+    });
+
+    after(done => cleanup(done));
+
     describe('/GET /auth/login', () => {
         it('it should fail logging user in when no basic auth', (done) => {
             chai.request(server)
@@ -21,7 +89,7 @@ describe('Auth', () => {
         it('it should succeed logging user in when correct basic auth', (done) => {
             chai.request(server)
                 .get(`${process.env.API_PREFIX}/auth/login`)
-                .set('Authorization', `Basic ${process.env.AUTH_BASIC}`)
+                .set('Authorization', `Basic ${basic}`)
                 .end((err, res) => {
                     res.should.have.status(200);
                     done();
@@ -34,7 +102,7 @@ describe('Auth', () => {
         before((done) => {
             chai.request(server)
                 .get(`${process.env.API_PREFIX}/auth/login`)
-                .set('Authorization', `Basic ${process.env.AUTH_BASIC}`)
+                .set('Authorization', `Basic ${basic}`)
                 .end((err, res) => {
                     token = res.body.token;
                     done();
@@ -52,7 +120,7 @@ describe('Auth', () => {
         it('it should succeed verifying user when bearer token present', (done) => {
             chai.request(server)
                 .get(`${process.env.API_PREFIX}/auth/verify`)
-                .set('Authorization', `Bearer ${token}`)
+                .set('Authorization', `Bearer ${loginToken}`)
                 .end((err, res) => {
                     res.should.have.status(200);
                     done();
